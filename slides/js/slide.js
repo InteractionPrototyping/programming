6297805e4f552cc0bc5156efa4c1c9480e16307a
AnnikaStumpf <stumpf.annika@gmail.com>
2015-10-03 14:42:16 +0200

    modified chapter A, deleted unnecessary css files

---
+++ slides/js/slide.js
@@ -0,0 +1,14 @@
+/* slides.js
+   Interaction Prototyping Block Course
+*/
+
+
+/*	A Web Applications			     */
+/*	  And Technology				 */
+
+$(document).ready(function(){
+    $(".answer").click(function(){
+        $("#Technology_Internet_question").hide();
+        $("#Technology_Internet_answer").show();
+    });
+});

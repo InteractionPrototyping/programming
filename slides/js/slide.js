9c5704829d2ede67b29355369b5a363792e5f375
AnnikaStumpf <stumpf.annika@gmail.com>
2015-10-08 18:43:52 +0200

    button disappears after being clicked

--- slides/js/slide.js
+++ slides/js/slide.js
@@ -53,4 +53,5 @@ function showMapPosition(position) {
     var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
     +latlon+"&zoom=14&size=400x300&sensor=false";
     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+    $("#showMapBtn").attr("display":"none");
 }

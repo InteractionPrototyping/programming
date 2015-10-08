cd43594873b8d53117378bb10d539ced3f123a5a
david-schopf <david.schopf@gmail.com>
2015-10-08 17:35:06 +0200

    Comment :)

--- exercises/filedate.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: david_000
- * Date: 08.10.2015
- * Time: 16:07
- */
-
-echo filemtime("index.html");
--- exercises/interaction.js
+++ exercises/interaction.js
@@ -1,5 +1,6 @@
 $(function () {
 
+	// Select the whole weather object, when clicking on it
 	$("#WeatherObject").click(function () {
 		window.getSelection().selectAllChildren(this);
 	});
